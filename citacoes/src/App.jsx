bc853a408f4b6bf6cb9745bfcb7bed7c380f0bab
Jefson Oliveira <kaironoliveira16@gmail.com>
2024-06-17 15:52:23 -0300

    feat: conclussão do projeto -Citações- -ReactJs- -HoraDeCodar-

--- citacoes/src/App.jsx
+++ citacoes/src/App.jsx
@@ -1,8 +1,19 @@
+import Citacao from "./components/Citacao";
+import citacoes from "./data";
+
+import { useState } from "react";
+
 function App() {
+  const [indice, setIndice] = useState(0);
+
+  const proximaCitacao = () => {
+    setIndice((indiceAtual) => (indiceAtual + 1) % citacoes.length);
+  };
 
   return (
-    <div className="App">
-      <h1>Testando bootstrap</h1>
+    <div className="container mt-5">
+      <Citacao texto={citacoes[indice].texto} autor={citacoes[indice].autor} />
+      <button className="btn btn-success mt-2" onClick={proximaCitacao}>Próxima citação</button>
     </div>
   );
 }
